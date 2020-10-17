32ac318194090ba31cea9b847820a642b0b21c16
HTO3 <helio_net@hotmail.com>
2020-10-17 20:58:08 -0300

    Fix unit test.

--- Hto3.DateTimeHelpers.Test/CalculateAge.cs
+++ Hto3.DateTimeHelpers.Test/CalculateAge.cs
@@ -13,11 +13,11 @@ namespace Hto3.DateTimeHelpers.Test
         [TestMethod]
         public void NormalUse()
         {
-            var today = DateTime.Today;
+            var today = new DateTime(2020, 10, 17, 20, 52, 0);
             var minusTenYearsOld = today.AddYears(10);
             var tenYearsOld = today.AddYears(-10);
 
-            Assert.AreEqual(DateTimeHelpers.CalculateAge(minusTenYearsOld), -10);
+            Assert.AreEqual(DateTimeHelpers.CalculateAge(minusTenYearsOld), -9);
             Assert.AreEqual(DateTimeHelpers.CalculateAge(tenYearsOld), 10);
             Assert.AreEqual(DateTimeHelpers.CalculateAge(today), 0);
         }
