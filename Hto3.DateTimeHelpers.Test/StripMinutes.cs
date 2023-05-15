0627fbce99e29df0be52d7776df60a390682ae11
Hélio Tibagí <helio_net@hotmail.com>
2023-05-15 14:05:47 -0300

    Add unit tests.

---
+++ Hto3.DateTimeHelpers.Test/StripMinutes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hto3.DateTimeHelpers.Test
+{
+    [TestClass]
+    public class StripMinutes
+    {
+        [TestMethod]
+        public void NormalUse()
+        {
+            //arrange
+            var input = new DateTime(2010, 10, 3, 12, 22, 7);
+            var expected = new DateTime(2010, 10, 3, 12, 0, 0);
+
+            //act
+            var actual = DateTimeHelpers.StripMinutes(input);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
